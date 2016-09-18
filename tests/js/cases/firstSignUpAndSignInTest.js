eae1557be2e64815b2fb67d8843105732a3b5732
moustachu <git@moustachu.net>
2016-09-18 16:04:53 +0200

    Travis init 101

---
+++ tests/js/cases/firstSignUpAndSignInTest.js
@@ -0,0 +1,45 @@
+module.exports = {
+  'DemocracyOS Signup then signin test' : function (browser) {
+    var TIMEOUT = 1000;
+
+    var indexPage = browser.page.index();
+    var signinPage = browser.page.signin();
+    var signupPage = browser.page.signup();
+    var signupConfirmPage = browser.page.signupConfirmation();
+
+    var USER = {
+      email: 'dev-' + new Date().getTime() + '@democracyos.eu',
+      password: 'devIsLove',
+      firstName: 'Dev',
+      lastName: 'Test'
+    }
+
+    indexPage
+      .navigate();
+
+    signinPage
+      .clickOnSignup()
+
+    signupPage
+      .signupWithUser(USER)
+
+    signupConfirmPage
+      .waitForElementVisible("@successMessage", TIMEOUT)
+      .assert.containsText("@successMessage", "Bienvenue")
+      .assert.containsText("@successMessage", USER.firstName)
+      .assert.containsText("@successMessage", USER.lastName)
+
+    indexPage
+      .clickOnSignin();
+
+    signinPage
+      .signinWithUser(USER)
+
+    indexPage
+      .waitForElementVisible("@userBadgeName", TIMEOUT)
+      .assert.containsText("@userBadgeName", USER.firstName.toUpperCase())
+
+    browser
+      .end();
+  }
+};
